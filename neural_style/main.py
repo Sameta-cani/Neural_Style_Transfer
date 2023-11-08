30dd4073cf8387a92d9f32b2039fc97a0e1f6d63
ParkSangJin <ticani0610@gmail.com>
2023-11-08 20:55:23 +0900

    중간 완료

--- neural_style/main.py
+++ neural_style/main.py
@@ -9,19 +9,9 @@ from PIL import Image
 import style
 import os
 import utils
-from torchvision import transforms
-import base64
 
-import streamlit as st
-import base64
-
-def download_file(file_content, file_name):
-    b64 = base64.b64encode(file_content).decode()
-    href = f'<a href="data:file/txt;base64,{b64}" download="{file_name}">Download {file_name}</a>'
-    st.markdown(href, unsafe_allow_html=True)
-
-
-
+# local test: "neural_style" -> "."
+# git test: "." -> "neural_style"
 test = "neural_style" # "."
 
 st.title('PyTorch Style Transfer')
@@ -42,10 +32,6 @@ if img == 'Image upload':
 	if img_file is not None:
 		utils.save_uploaded_file(test + "/images/content-images/", img_file)
 		img = img_file.name
-# if img_file is not None:
-
-# local test: "neural_style" -> "."
-# git test: "." -> "neural_style"
 
 model = test + "/saved_models/" + style_name + ".pth"
 input_image = test + "/images/content-images/" + img
@@ -63,19 +49,16 @@ if clicked:
     data_img = data[0].clone().clamp(0, 255).numpy()
     data_img = data_img.transpose(1, 2, 0).astype("uint8")
     data_img = Image.fromarray(data_img)
+
     st.write('### Output image:')
-    # image = Image.open(output_image)
     st.image(data_img, width=400)
-    tmp = style_name + "-" + img
-    data_img.save(tmp)
-    with open(tmp, "rb") as file:
+
+    output_path = style_name + "-" + img
+    data_img.save(output_path)
+    with open(output_path, "rb") as file:
         btn = st.download_button(
             label="Download image",
             data=file,
-            file_name=tmp,
+            file_name=output_path,
             mime="image/png"
           )
-
-'''    if save_button:
-        download_file(data[0].numpy(), style_name + "-" + img)
-        st.write('save!')'''
